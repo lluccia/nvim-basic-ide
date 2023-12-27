fb934f9e33ea3344fc6ed2380484e859279f379a
Leandro Conca <lluccia@gmail.com>
2023-12-26 23:47:55 -0300

    higher contrast color for window separators

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -1,6 +1,13 @@
+require("tokyonight").setup({
+  on_colors = function(colors)
+    colors.border = "#565f89"
+  end
+})
+
 local colorscheme = "tokyonight-night"
 
 local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 if not status_ok then
   return
 end
+
