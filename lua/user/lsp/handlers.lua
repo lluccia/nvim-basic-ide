a384e9380e3996997495ad348a9e2056fbc12fbc
Leandro Conca <leandro.luccia@spcbrasil.com.br>
2025-09-24 09:30:28 -0300

    fixes some deprecated methods and errors during startup

--- lua/user/lsp/handlers.lua
+++ lua/user/lsp/handlers.lua
@@ -10,21 +10,17 @@ M.capabilities.textDocument.completion.completionItem.snippetSupport = true
 M.capabilities = cmp_nvim_lsp.default_capabilities()
 
 M.setup = function()
-	local signs = {
-		{ name = "DiagnosticSignError", text = "" },
-		{ name = "DiagnosticSignWarn", text = "" },
-		{ name = "DiagnosticSignHint", text = "󰌶" },
-		{ name = "DiagnosticSignInfo", text = "" },
-	}
-
-	for _, sign in ipairs(signs) do
-		vim.fn.sign_define(sign.name, { texthl = sign.name, text = sign.text, numhl = "" })
-	end
-
 	local config = {
 		virtual_text = false, -- disable virtual text
 		signs = {
 			active = signs, -- show signs
+
+      text = {
+        [vim.diagnostic.severity.ERROR] = "",
+        [vim.diagnostic.severity.WARN]  = "",
+        [vim.diagnostic.severity.INFO]  = "",
+        [vim.diagnostic.severity.HINT]  = "",
+      },
 		},
 		update_in_insert = true,
 		underline = true,
