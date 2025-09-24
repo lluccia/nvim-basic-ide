e46f941d3351abb5c918c2a740ac37b39d3442c1
Leandro Conca <leandro.luccia@spcbrasil.com.br>
2025-09-24 09:27:53 -0300

    jdtls command line customization and runtimes configuration

---
+++ after/lsp/jdtls.lua
@@ -0,0 +1,63 @@
+local java_runtimes = require("user.lsp.java-runtimes.mise")
+
+local home = os.getenv "HOME"
+local project_name = vim.fn.fnamemodify(vim.fn.getcwd(), ":p:h:t")
+local workspace_dir = home .. "/.jdtls/" .. project_name
+
+---@type vim.lsp.Config
+return {
+  cmd = {
+    java_runtimes.java_versions[21].path .. "/bin/java",
+    -- "-Declipse.application=org.eclipse.jdt.ls.core.id1",
+    -- "-Dosgi.bundles.defaultStartLevel=4",
+    -- "-Declipse.product=org.eclipse.jdt.ls.core.product",
+    "-Dlog.protocol=true",
+    "-Dlog.level=INFO",
+    "-javaagent:" .. home .. "/.local/share/nvim/mason/packages/jdtls/lombok.jar",
+    "-XX:+UseParallelGC", "-XX:GCTimeRatio=4", "-XX:AdaptiveSizePolicyWeight=90", "-Dsun.zip.disableMemoryMapping=true", -- from default jdt vmargs
+    "-Xms512m", "-Xmx4g",
+    "--add-modules=ALL-SYSTEM",
+    "--add-opens", "java.base/java.util=ALL-UNNAMED",
+    "--add-opens", "java.base/java.lang=ALL-UNNAMED",
+    "-jar", vim.fn.glob(home .. "/.local/share/nvim/mason/packages/jdtls/plugins/org.eclipse.equinox.launcher_*.jar"),
+    "-configuration", home .. "/.local/share/nvim/mason/packages/jdtls/config_linux",
+    "-data", workspace_dir,
+  },
+  settings = {
+    java = {
+      eclipse = {
+        downloadSources = true,
+      },
+      configuration = {
+        updateBuildConfiguration = "interactive",
+        runtimes = java_runtimes.runtimes,
+      },
+      maven = {
+        downloadSources = true,
+      },
+      implementationsCodeLens = {
+        enabled = true,
+      },
+      referencesCodeLens = {
+        enabled = true,
+      },
+      references = {
+        includeDecompiledSources = true,
+      },
+      format = {
+        enabled = true,
+      },
+    },
+    completion = {
+      favoriteStaticMembers = {
+        "org.assertj.core.api.Assertions.assertThat",
+        "java.util.Objects.requireNonNull",
+        "java.util.Objects.requireNonNullElse",
+        "org.mockito.Mockito.*",
+      },
+    },
+  },
+  init_options = {
+    bundles = {},
+  },
+}
